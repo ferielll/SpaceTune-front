2f587e652da869f46db4210f84ffe8ca5f1a6772
Mourad Ben Rahal <mouradbenrahal12@gmail.com>
2022-03-01 20:40:00 +0100

    tailwind

--- src/components/Modal/ConfirmModal.jsx
+++ src/components/Modal/ConfirmModal.jsx
@@ -84,7 +84,12 @@ export default function ConfirmModal({
               <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                 <button
                   type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  className={`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 
+                  ${
+                    type === "submit"
+                      ? "bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 "
+                      : "bg-red-600 hover:bg-red-700 focus:ring-red-500 "
+                  } text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2  sm:ml-3 sm:w-auto sm:text-sm`}
                   onClick={() => onClickConfirm()}
                 >
                   {confirmButton}
